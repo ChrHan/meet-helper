72f8a35e330e4033830987358560685a73a636b4
Muhammad Saiful Islam <muhammad@saiful.web.id>
2023-03-23 13:47:44 +0700

    Detect when user joined a call

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,19 @@
-console.log('I am working!');
+const body = document.body;
+const observer = new MutationObserver(mutations => {
+    for (let mutation of mutations) {
+        if (mutation.type != 'childList')
+            continue;
+
+        for (let node of mutation.addedNodes) {
+            if (node.textContent == 'call_end'
+                && node.parentElement?.classList.contains('google-material-icons')) {
+                console.log('Joined a call');
+            }
+        }
+    }
+});
+
+observer.observe(body, {
+    childList: true,
+    subtree: true
+});
